2f730a1af82ae54d3f6f1ca7bb09a7d90d04d3bc
tembatsurri <tarantem@icloud.com>
2026-03-16 17:17:38 -0700

    finished all preprocessing functions + configured it with the data set

--- README.md
+++ README.md
@@ -2,22 +2,45 @@ First ML pipeline plan
 
 Model Type : Classification & Segmentation
 
-We will train a segmentation model with 6 output channels, one for each class/label. Since each output mask already indicates whether a label is present and where it is located, image-level classification can be derived from the predicted masks by checking whether each mask contains a sufficiently large positive region. This avoids adding a separate classification head in the first version of the pipeline.
+We will train a segmentation model with 5-8 output channels, one for each class/label. Since each output mask already indicates whether a label is present and where it is located, image-level classification can be derived from the predicted masks by checking whether each mask contains a sufficiently large positive region. This avoids adding a separate classification head in the first version of the pipeline.
 
 TRAINING STEPS
 
-Preprocessing Data:
-- have all the data file names organized in a list/ or other convenient data strucutre
-- batch the data paths & organize accordingly to training and testing data.
+1. 
+Keep a list of meta data for samples
+Each sample should contain paths for:
+- the image
+- the 5 anomaly masks
+- boundary / valid mask if needed
 
-Preprocess Batch Function
+Sample will be stored as a dictionary / hashmap:
+
+{
+    "rgb": ".../field_images/rgb/1C11HIBEV_....png",
+    "nir": ".../field_images/nir/1C11HIBEV_....png",
+    "masks": {
+        "planter_skip": ".../field_labels/planter_skip/1C11HIBEV_....png",
+        "water": ".../field_labels/water/1C11HIBEV_....png",
+        "weed_cluster": ".../field_labels/weed_cluster/1C11HIBEV_....png",
+        "waterway": ".../field_labels/waterway/1C11HIBEV_....png",
+        "nutrient_deficiency": ".../field_labels/nutrient_deficiency/1C11HIBEV_....png"
+    },
+    "valid_mask": ".../field_masks/1C11HIBEV_....png"
+}
+
+2. Batch Metadata
+We will manually batch metadata
+
+
+3. Preprocess metadata per batch
+
 Create a function A that for each image in a batch
 - load images into matrices
 - make sure images are of 512 x 512
 - normalize entries
 - augment the matrices(make more variation)
 - keep the labels matched(answerkey) with the images
-In this case each image will have 6 binary masks, one for each label we are testing.
+In this case each image will have 5 binary masks, one for each label we are testing.
 - returns training images as matrixes corresponding to their answer(labelled) matrix, perhaps in a dictionary/hashmap.
 
 TRAINING:
@@ -27,4 +50,4 @@ Loss function :
 Iterate through the batches over epochs, in each iteration adjust the model weights using the loss function & other calcultions.
 
 Other calculations include: 
-- to do later
+- to do later ?????
--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,91 @@
 import tensorflow as tf
 import numpy as np
 
-NUM_ANOMALIES = 6
+NUM_ANOMALIES = 5
 IMAGE_SIZE = 512
 
 ANOMALY_NAMES = [
-    "cloud_shadow",
-    "double_plant",
     "planter_skip",
-    "standing_water",
-    "waterway",
+    "water",
     "weed_cluster",
+    "waterway",
+    "nutrient_deficiency"
 ]
 
-#collect all images paths under data folder and store the path
-dataset = tf.data.Dataset.list_files("data/*.jpg", shuffle = False)
+# need the training image paths stored as in the readme
+dataset = []
 
 
-def load_image(image_path):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels =3)
-    #image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
-    image = tf.cast(image, tf.float32)/255.0
+
+
+
+
+
+
+def load_image(rgb_path, nir_path):
+    rgb_bytes = tf.io.read_file(rgb_path)
+    rgb = tf.image.decode_png(rgb_bytes, channels=3)
+    rgb = tf.cast(rgb, tf.float32) / 255.0
+    rgb = tf.ensure_shape(rgb, [IMAGE_SIZE, IMAGE_SIZE, 3])
+
+    nir_bytes = tf.io.read_file(nir_path)
+    nir = tf.image.decode_png(nir_bytes, channels=1)
+    nir = tf.cast(nir, tf.float32) / 255.0
+    nir = tf.ensure_shape(nir, [IMAGE_SIZE, IMAGE_SIZE, 1])
+
+    image = tf.concat([rgb, nir], axis=-1)   # shape: (512, 512, 4)
     return image
+
+def load_binary_mask(mask_path):
+    mask_bytes = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask_bytes, channels=1)
+    mask = tf.cast(mask > 0, tf.float32)
+    mask = tf.ensure_shape(mask, [IMAGE_SIZE, IMAGE_SIZE, 1])
+    return mask
+
+def load_all_masks(mask_paths):
+    masks = [load_binary_mask(path) for path in mask_paths]
+    masks = tf.concat(masks, axis=-1)   # shape: (512, 512, 5)
+    return masks
+
+def load_valid_mask(valid_mask_path):
+    mask_bytes = tf.io.read_file(valid_mask_path)
+    valid_mask = tf.image.decode_png(mask_bytes, channels=1)
+    valid_mask = tf.cast(valid_mask > 0, tf.float32)
+    valid_mask = tf.ensure_shape(valid_mask, [IMAGE_SIZE, IMAGE_SIZE, 1])
+    return valid_mask
+
+#augmentation adds more variability to the training data
+def augment(image, masks, valid_mask=None):
+    do_hflip = tf.random.uniform(()) > 0.5
+    do_vflip = tf.random.uniform(()) > 0.5
+
+    image = tf.cond(do_hflip, lambda: tf.image.flip_left_right(image), lambda: image)
+    masks = tf.cond(do_hflip, lambda: tf.image.flip_left_right(masks), lambda: masks)
+
+    image = tf.cond(do_vflip, lambda: tf.image.flip_up_down(image), lambda: image)
+    masks = tf.cond(do_vflip, lambda: tf.image.flip_up_down(masks), lambda: masks)
+
+    if valid_mask is not None:
+        valid_mask = tf.cond(do_hflip, lambda: tf.image.flip_left_right(valid_mask), lambda: valid_mask)
+        valid_mask = tf.cond(do_vflip, lambda: tf.image.flip_up_down(valid_mask), lambda: valid_mask)
+
+    return image, masks, valid_mask
+
+#complete preprocess 
+def preprocess_sample(rgb_path, nir_path, mask_paths, valid_mask_path=None, training=True):
+    image = load_image(rgb_path, nir_path)
+    masks = load_all_masks(mask_paths)
+
+    valid_mask = None
+    if valid_mask_path:
+        valid_mask = load_valid_mask(valid_mask_path)
+
+    if training:
+        image, masks, valid_mask = augment(image, masks, valid_mask)
+
+    if valid_mask is not None:
+        return image, masks, valid_mask
+    else:
+        return image, masks
 
